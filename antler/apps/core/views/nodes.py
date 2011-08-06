10cc1164567c7dc3e93cd72be0dead3c4f652e86
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-08-06 02:18:02 +0100

    added concepts, events and object placeholders

--- antler/apps/core/views/nodes.py
+++ antler/apps/core/views/nodes.py
@@ -1,6 +1,47 @@
 from django.views.generic.base import TemplateView 
+from core.models import Person
+from django.shortcuts import get_object_or_404
 
 class PersonView(TemplateView):
 
     template_name = "nodes/person.html"
 
+    def get_context_data(self, pk):
+        person = get_object_or_404(Person, pk=pk)
+        return {
+                "person":person,
+               }
+        
+        
+class EventView(TemplateView):
+    
+    template_name = "nodes/event.html"
+    
+    def get_context_data(self, pk):
+        event = get_object_or_404(Event, pk=pk)
+        return {
+                "event":event,
+               }
+
+        
+class ConceptView(TemplateView):
+    
+    template_name = "nodes/concept.html"
+    
+    def get_context_data(self, pk):
+        concept = get_object_or_404(Concept, pk=pk)
+        return {
+                "concept":concept,
+               }
+
+
+class ObjectView(TemplateView):
+    
+    template_name = "nodes/object.html"
+    
+    def get_context_data(self, pk):
+        object = get_object_or_404(Object, pk=pk)
+        return {
+                "object":object,
+               }
+        
--- antler/urls.py
+++ antler/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
     url(r'^import/', importer.ImportView.as_view(), name="import"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^person/(?P<pk>\d+)/', nodes.PersonView.as_view(), name="person"),
+    url(r'^concept/(?P<pk>\d+)/', nodes.ConceptView.as_view(), name="concept"),
+    url(r'^event/(?P<pk>\d+)/', nodes.EventView.as_view(), name="event"),
+    url(r'^object/(?P<pk>\d+)/', nodes.ObjectView.as_view(), name="object"),
 )
