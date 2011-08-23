c547edb795518c818638a2b680c63f9c9284644b
James Coglan <jcoglan@gmail.com>
2011-08-22 17:03:37 -0700

    Change file server address.

--- tools/wikibrowser/environment.rb
+++ tools/wikibrowser/environment.rb
@@ -1,7 +1,7 @@
 require 'active_record'
 
 # Server where you can get chunks of the big Wikipedia dump
-FILE_HOST = 'http://10.0.0.51:9292/'
+FILE_HOST = 'http://psql.fort:9292/'
 
 # Database server articles and links
 ActiveRecord::Base.establish_connection(
