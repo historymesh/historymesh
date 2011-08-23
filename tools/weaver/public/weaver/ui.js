f312c9d37e7a320753e1cd06cfd52bceca3c1afa
Blaine Cook <romeda@gmail.com>
2011-08-23 22:24:23 +0100

    add saved class to all links that are saved

--- tools/weaver/public/weaver/ui.js
+++ tools/weaver/public/weaver/ui.js
@@ -4,15 +4,21 @@ var findSaved = function () {
   storage.getSavedArticles(function(articles) {
     var names = articles.map(function(a) { return a.name });
 
-    $('a.save').each(function(i, link) {
+    $('a').each(function(i, link) {
       var href = $(link).attr('href');
+
+      if (!href) return;
 
       var slug = href.replace(/^.*\/wiki\/(.*)$/, '$1'),
           name = decodeURIComponent(slug);
 
       if (names.indexOf(name) < 0) return;
 
-      $(link).addClass('saved').html('Saved!');
+      $(link).addClass('saved')
+
+      if ($(link).html == 'Save') {
+        $(link).html('Saved!');
+      }
     });
   });
 };
