f459ced92d7c6ea5543660b0facca4ca30787d80
Blaine Cook <romeda@gmail.com>
2011-08-23 21:05:35 +0100

    continued refactoring

---
+++ tools/weaver/public/weaver/ui.js
@@ -0,0 +1,26 @@
+var storage = new Weaver.Storage();
+
+var findSaved = function () {
+  storage.getSavedArticles(function(articles) {
+    var names = articles.map(function(a) { return a.name });
+
+    $('a.save').each(function(i, link) {
+      var href = $(link).attr('href');
+
+      var slug = href.replace(/^.*\/wiki\/(.*)$/, '$1'),
+          name = decodeURIComponent(slug);
+
+      if (names.indexOf(name) < 0) return;
+
+      $(link).addClass('saved').html('Saved!');
+    });
+  });
+};
+
+var urlToArticle = function (uri) {
+  var slug = uri.replace(/^.*\/wiki\/(.*)$/, '$1'),
+      name = decodeURIComponent(slug),
+      article = new Weaver.Article(name);
+
+  return article;
+}
