e8e96b7d1c7c5047025ce83229fe56eff3744634
George Brocklehurst <george.brocklehurst@gmail.com>
2011-08-23 02:39:06 +0100

    Go go gadget Django Admin

--- antler/configs/settings.py
+++ antler/configs/settings.py
@@ -104,10 +104,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'core',
     'south',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
