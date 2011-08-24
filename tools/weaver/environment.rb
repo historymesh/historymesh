309628d61b41f58c6c5366f2185d3e61bf44035e
James Coglan <jcoglan@gmail.com>
2011-08-24 06:09:32 -0700

    Switch data server for Weaver.

--- tools/weaver/environment.rb
+++ tools/weaver/environment.rb
@@ -5,7 +5,7 @@ require 'wikicloth'
 require 'wikitext'
 
 # Server to get Wikipedia article data
-CONTENT_HOST = '10.0.0.53:5001'
+CONTENT_HOST = '10.0.0.66:5001'
 
 root = File.dirname(__FILE__)
 require root + '/models/article'
