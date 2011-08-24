8c23dd63daadafb4d7abe918dca398ca8456c32f
Mark Norman Francis <norm@cackhanded.net>
2011-08-24 16:47:50 +0100

    Add stories and a timeline holder to homepage.

--- antler/apps/homepage/views.py
+++ antler/apps/homepage/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from core.models import Story
 
 
 class HomepageView(TemplateView):
     template_name = 'homepage.html'
+    
+    def get_context_data(self):
+        return {
+            'stories': Story.objects.all()
+        }
