7ce44635d60e6f567efb5fa64f17e8354b04a63e
Ryan Alexander <rnalexander@gmail.com>
2011-08-24 11:37:46 +0100

    Adding template path

--- antler/configs/settings.py
+++ antler/configs/settings.py
@@ -1,4 +1,6 @@
 # Django settings for antler project.
+import os
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -90,12 +92,12 @@ MIDDLEWARE_CLASSES = (
     'lib.exception_console.ExceptionConsoleMiddleware',
 )
 
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
+
 ROOT_URLCONF = 'antler.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
