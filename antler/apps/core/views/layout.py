7a413469fb6f27b0aa008e20331bf92906b6f9f1
Andrew Godwin <andrew@aeracode.org>
2011-08-24 17:21:13 +0100

    Progress on the layout stuff

--- antler/apps/core/views/layout.py
+++ antler/apps/core/views/layout.py
@@ -1,11 +1,12 @@
 import pprint
+from collections import deque
 from django.http import HttpResponse
 from core.models import Edge, Node
 
 
 def layout_view(request):
     nodes = NodeLayoutEngine().lay_out()
-    return HttpResponse(("%r" % nodes).replace("<", "&lt;"))
+    return HttpResponse(("%r" % (nodes,)).replace("<", "&lt;"))
 
 
 class NodeLayoutEngine(object):
@@ -13,65 +14,72 @@ class NodeLayoutEngine(object):
     Lays out nodes in a rough time-like order.
     """
 
-    max_per_era = 6
+    min_per_era = 3
     era_sizes = [1, 5, 10, 50, 100, 500, 1000]
 
     def lay_out(self):
         # First, fetch a list of all the nodes
         self.load_nodes()
-        # Then, march down the nodes grouping them into
-        # rounded historical eras.
-        self.decide_eras()
+        self.construct_strings()
         # Return the layout
-        return {}
+        return self.nodes, self.strings
 
     def load_nodes(self):
         """
         Fetches all of the nodes in date order.
         """
         # Fetch all of the nodes into a list (partially sorted)
-        self.nodes = []
-        for node_class in Node.all_child_classes():
-            self.nodes.extend(
-                node_class.objects.filter(
-                    timeline_date__isnull = False,
-                ).order_by("timeline_date")
-            )
-        # Finish sorting them
-        self.nodes.sort(key=lambda node: node.timeline_date)
+        self.nodes = set()
+        for edge in Edge.objects.filter(story__isnull=False):
+            self.nodes.add(edge.subject)
+            self.nodes.add(edge.object)
+        # Sort them
+        self.nodes = sorted(self.nodes, key=lambda node: node.timeline_date)
 
-    def year_to_era(self, year, era_size):
-        return year // era_size * era_size
+    def construct_strings(self):
+        """
+        Works out the strings of single-pathed nodes.
+
+        A string is a group of nodes that form a singly-linked
+        path. The string ends whenever a node has more than one
+        successor; that node is included as the end of the string,
+        and the successors form the start of new strings.
+        """
+        # Strings. Stored by first node.
+        self.strings = {}
+        # Queue-based string processor
+        queue = deque()
+        edges = list(Edge.objects.filter(story__isnull=False))
+        edges_by_subject = {}
+        for edge in edges:
+            edges_by_subject.setdefault(edge.subject, []).append(edge)
+        # Start with all nodes with no incoming edges
+        for node in set(self.nodes) - set(edge.object for edge in edges):
+            queue.append(node)
+        # Process!
+        while queue:
+            node = queue.popleft()
+            # Start a new string, follow it till it splits
+            string = [node]
+            while True:
+                # If there's one outgoing edge, continue the string
+                if len(edges_by_subject[node]) == 1:
+                    node = edges_by_subject[node][0].object
+                    string.append(node)
+                # If there's none, stop here.
+                elif len(edges_by_subject[node]) == 0:
+                    break
+                # If there's 2 or more, add them to the queue
+                else:
+                    for other_node in edges_by_subject[node]:
+                        queue.append(other_node)
+                    break
+            # Save the string
+            self.strings[string[0]] = string
 
     def decide_eras(self):
         """
         Goes down the list of nodes and decides what historical
         eras to use for grouping.
         """
-        # Go through the nodes and bucket them by all possible era
-        # sizes.
-        buckets = dict((era_size, {}) for era_size in self.era_sizes)
-        for node in self.nodes:
-            for era_size in self.era_sizes:
-                era = self.year_to_era(node.timeline_date.year, era_size)
-                buckets[era_size].setdefault(era, []).append(node)
-        pprint.pprint(buckets)
-        # Go through the possible buckets and pick the ones that are
-        # closest to the optimal size
-        earliest = min(buckets[1].keys())
-        latest = max(buckets[1].keys())
-        boundaries = []
-        current = earliest
-        while current <= latest:
-            # Find the best-sized era
-            for era_size in reversed(sorted(self.era_sizes)):
-                # Work out the size this bucket would be
-                era = self.year_to_era(current, era_size)
-                bucket_size = len(buckets[era_size][era])
-                # If it's available, and best, use it
-                if bucket_size < self.max_per_era and \
-                   (not buckets or buckets[-1] < era):
-                    break
-            boundaries.append(era)
-        pprint.pprint(boundaries)
-
+        
