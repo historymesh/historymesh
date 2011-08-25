e981ca3c6ac5fedfec96864fd00207e4d5776504
Richard Boulton <richard@tartarus.org>
2011-08-25 20:42:50 +0100

    Get rid of silly stuff to handle restpose not being installed

--- antler/apps/search/search.py
+++ antler/apps/search/search.py
@@ -1,18 +1,8 @@
 
 from django.conf import settings
-
-try:
-    from restpose import Server
-except ImportError:
-    # Allow the site to work without search
-    class Server(object):
-        fake = True
-        def __init__(self, *args, **kwargs):
-            pass
+from restpose import Server
 
 server = Server(settings.RESTPOSE_CONFIG['uri'])
 
 def index(obj):
-    if hasattr(server, 'fake'):
-        raise RuntimeError("restpose client not installed")
-    print obj
+    print obj.search_data
