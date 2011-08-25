1b377fd01b4ef962d265f43c40487d162b37f2b2
David Thompson <mail@fatbusinessman.com>
2011-08-25 15:28:00 +0100

    Add a virtualenv init script.

---
+++ init_virtualenv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: init_virtualenv.sh project [project..]"
+    exit 1
+fi
+
+for project_name in $*; do
+    env_name="${project_name}_ve"
+
+    if [[ -e $env_name ]]; then
+	echo "Looks like you've already got a ${project_name} virtualenv here."
+	echo "I'm not going to mess with it."
+	exit 1
+    fi
+
+    echo "Setting up virtualenv for ${project_name}"
+    virtualenv $env_name
+    echo "Installing requirements"
+    pip install -E $env_name --use-mirrors --mirrors=http://pypi.fort/ -r ${project_name}/requirements.txt
+done
