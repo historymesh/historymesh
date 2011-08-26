143afaeb083ca79fe37d373f7d47a9e9990a2ff4
David Thompson <mail@fatbusinessman.com>
2011-08-26 10:36:37 +0100

    Detab.

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -11,9 +11,9 @@ for project_name in $*; do
     env_name="$(basename $project_name)_ve"
 
     if [[ -e $env_name ]]; then
-	echo "Looks like you've already got a ${project_name} virtualenv here."
-	echo "I'm not going to mess with it."
-	exit 1
+    echo "Looks like you've already got a ${project_name} virtualenv here."
+    echo "I'm not going to mess with it."
+    exit 1
     fi
 
     echo "Setting up virtualenv for ${project_name}"
