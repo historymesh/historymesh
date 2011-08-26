14e659b1cd89627c9981e17f2b7963c3a7d42cd1
Ash Berlin <ash@ash-vm.(none)>
2011-08-25 20:34:08 -0700

    Have the map display on the left hand side of the node page use real data

--- antler/apps/core/views/nodes.py
+++ antler/apps/core/views/nodes.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.views.generic.base import TemplateView, View
-from core.models import Person, Concept, Event, Object, Story
+from core.models import Person, Concept, Event, Object, Story, Edge
 from django.shortcuts import HttpResponseRedirect as Redirect, get_object_or_404
 import random
 
@@ -31,12 +31,74 @@ class NodeView(TemplateView):
         if len(story_prev) > 0:
             story_prev = story_prev[0]
 
+        nodes,edges = self.story_node_map(story)
+
+        # Group the nodes by century(ish) for <noscript> output
+        #
+        # The groups we want are:
+        groups = {
+           -4000: { "label": ' ', "nodes": [], "display_mark": False },
+               0: { "label": 'BC/BCE', "nodes": [] },
+            1000: { "nodes": [] },
+            1200: { "nodes": [] },
+            1600: { "nodes": [] },
+            1700: { "nodes": [] },
+            1800: { "nodes": [] },
+            1900: { "nodes": [] },
+            9999: { "nodes": [] },
+        }
+
+        keys = sorted(groups.keys())
+        group = groups[keys[0]]
+        marks = []
+        i = 0
+        node_separation = 45
+        last_node = None
+        for node in nodes:
+            while len(keys) and node.timeline_date > keys[0]:
+                if last_node and group.get("display_mark") != False:
+
+                    if not len(group.get("nodes")):
+                        marks.pop()
+                    since = keys[0] - last_node.timeline_date
+                    diff = node.timeline_date - last_node.timeline_date
+                    pos = last_node.position + (since / float(diff)) * node_separation
+                    label = group.get("label")
+                    if label == None:
+                        label = keys[0]
+                    marks.append({
+                        "label": label,
+                        "position": pos
+                    })
+                keys = keys[1:]
+                group = groups[keys[0]]
+            node.position = node_separation * i
+            group.get("nodes").append(node)
+            last_node = node
+            i += 1
+
         return {
             "subject": instance,
             "incoming": instance.incoming().by_verb(),
             "outgoing": instance.outgoing().by_verb(),
             "story": story,
+            "map_nodes": nodes,
+            "map_edges": edges,
+            "map_marks": marks,
+            "century_grouped_nodes": groups,
         }
+
+    def story_node_map(self, story):
+        nodes = set()
+        edges = set()
+        for edge in Edge.objects.filter(story=story):
+            nodes.add(edge.subject)
+            nodes.add(edge.object)
+            edges.add(edge)
+        # Sort them
+        nodes = sorted(nodes, key=lambda node: node.timeline_date)
+        return nodes,edges
+
 
 
 class PersonView(NodeView):
